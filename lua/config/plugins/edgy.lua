f503619e95fe2c4c3368b04c75b1806cbdf7c82b
Ibrahim Erturk <me@home-lab-bsd.local>
2025-04-28 00:09:57 +0300

    Edgy config - removed buggy components

--- lua/config/plugins/edgy.lua
+++ lua/config/plugins/edgy.lua
@@ -1,54 +1,60 @@
 return {
-  "folke/edgy.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.opt.laststatus = 3
-    vim.opt.splitkeep = "screen"
-  end,
-  opts = {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.opt.laststatus = 3
+        vim.opt.splitkeep = "screen"
+    end,
+    opts = {
     top = {
-      -- Add bufferline to the top layout
-      {
-        ft = "bufferline",
-        title = "Bufferline",
-        size = { height = 1 }, -- Set height for bufferline
-        filter = function(buf)
-          return vim.bo[buf].buftype == "" -- Only show normal buffers
-        end,
-      },
+        -- Add bufferline to the top layout
+        {
+            ft = "bufferline",
+            title = "Bufferline",
+            size = { height = 1 }, -- Set height for bufferline
+            filter = function(buf)
+                return vim.bo[buf].buftype == "" -- Only show normal buffers
+            end,
+        },
     },
     bottom = {
-      -- toggleterm / lazyterm at the bottom with a height of 40% of the screen
-      {
-        ft = "toggleterm",
-        size = { height = 0.4 },
-        -- exclude floating windows
-        filter = function(buf, win)
-          return vim.api.nvim_win_get_config(win).relative == ""
-        end,
-      },
-      {
-        ft = "lazyterm",
-        title = "LazyTerm",
-        size = { height = 0.4 },
-        filter = function(buf)
-          return not vim.b[buf].lazyterm_cmd
-        end,
-      },
-      "Trouble",
-      { ft = "qf", title = "QuickFix" },
-      {
-        ft = "help",
-        size = { height = 20 },
-        -- only show help buffers
-        filter = function(buf)
-          return vim.bo[buf].buftype == "help"
-        end,
-      },
-      { ft = "spectre_panel", size = { height = 0.4 } },
+        -- toggleterm / lazyterm at the bottom with a height of 40% of the screen
+        {
+            ft = "toggleterm",
+            size = { height = 0.4 },
+            -- exclude floating windows
+            filter = function(buf, win)
+                return vim.api.nvim_win_get_config(win).relative == ""
+            end,
+        },
+        {
+            ft = "lazyterm",
+            title = "LazyTerm",
+            size = { height = 0.4 },
+            filter = function(buf)
+                return not vim.b[buf].lazyterm_cmd
+            end,
+        },
+        "Trouble",
+        {
+            ft = "qf",
+            title = "QuickFix" 
+        },
+        {
+            ft = "help",
+            size = { height = 20 },
+            -- only show help buffers
+            filter = function(buf)
+                return vim.bo[buf].buftype == "help"
+            end,
+        },
+        {
+            ft = "spectre_panel",
+            size = { height = 0.4 }
+        },
     },
     left = {
-      -- Neo-tree filesystem always takes half the screen height
+--      -- Neo-tree filesystem always takes half the screen height
       {
         title = "Neo-Tree",
         ft = "neo-tree",
@@ -57,45 +63,45 @@ return {
         end,
         size = { height = 0.5 },
       },
-      {
-        title = "Neo-Tree Git",
-        ft = "neo-tree",
-        filter = function(buf)
-          return vim.b[buf].neo_tree_source == "git_status"
-        end,
-        pinned = true,
-        collapsed = true, -- show window as closed/collapsed on start
-        open = "Neotree position=right git_status",
-      },
-      {
-        title = "Neo-Tree Buffers",
-        ft = "neo-tree",
-        filter = function(buf)
-          return vim.b[buf].neo_tree_source == "buffers"
-        end,
-        pinned = true,
-        collapsed = true, -- show window as closed/collapsed on start
-        open = "Neotree position=top buffers",
-      },
-      {
-        title = function()
-          local buf_name = vim.api.nvim_buf_get_name(0) or "[No Name]"
-          return vim.fn.fnamemodify(buf_name, ":t")
-        end,
-        ft = "Outline",
-        pinned = true,
-        open = "SymbolsOutlineOpen",
-      },
-      -- any other neo-tree windows
-      "neo-tree",
+--       {
+--        title = "Neo-Tree Git",
+--        ft = "neo-tree",
+--        filter = function(buf)
+--          return vim.b[buf].neo_tree_source == "git_status"
+--        end,
+--        pinned = true,
+--        collapsed = true, -- show window as closed/collapsed on start
+--        open = "Neotree position=right git_status",
+--      },
+--      {
+--        title = "Neo-Tree Buffers",
+--        ft = "neo-tree",
+--        filter = function(buf)
+--          return vim.b[buf].neo_tree_source == "buffers"
+--        end,
+--        pinned = true,
+--        collapsed = true, -- show window as closed/collapsed on start
+--        open = "Neotree position=top buffers",
+--      },
+--      {
+--        title = function()
+--          local buf_name = vim.api.nvim_buf_get_name(0) or "[No Name]"
+--          return vim.fn.fnamemodify(buf_name, ":t")
+--        end,
+--        ft = "Outline",
+--        pinned = true,
+--        open = "SymbolsOutlineOpen",
+--      },
+--      -- any other neo-tree windows
+--      -- "neo-tree",
     },
-    right = {
-      -- Add Copilot Chat to the right layout
-      {
-        ft = "copilot-chat",
-        title = "Copilot Chat",
-        size = { width = 50 }, -- Set width for Copilot Chat
-      },
+        right = {
+            -- Add Copilot Chat to the right layout
+            {
+                ft = "copilot-chat",
+                title = "Copilot Chat",
+                size = { width = 50 }, -- Set width for Copilot Chat
+            },
+        },
     },
-  },
 }
--- lua/config/plugins/tokyonight.lua
+++ lua/config/plugins/tokyonight.lua
@@ -1,6 +1,6 @@
 return {
     "folke/tokyonight.nvim", -- VS Code-like color scheme
-    enabled = false,
+    enabled = true,
     priority = 1000, -- Ensure it loads first
     config = function()
         vim.cmd("colorscheme tokyonight") -- Set the color scheme
