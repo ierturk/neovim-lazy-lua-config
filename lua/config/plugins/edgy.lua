e7a013c47aed8b4839cf3f29906e706c06cecea8
Ibrahim Erturk <me@home-lab-bsd.local>
2025-04-28 00:47:13 +0300

    Edgy config - disabled pinned property

--- lua/config/plugins/edgy.lua
+++ lua/config/plugins/edgy.lua
@@ -54,46 +54,45 @@ return {
         },
     },
     left = {
---      -- Neo-tree filesystem always takes half the screen height
-      {
-        title = "Neo-Tree",
-        ft = "neo-tree",
-        filter = function(buf)
-          return vim.b[buf].neo_tree_source == "filesystem"
-        end,
-        size = { height = 0.5 },
-      },
---       {
---        title = "Neo-Tree Git",
---        ft = "neo-tree",
---        filter = function(buf)
---          return vim.b[buf].neo_tree_source == "git_status"
---        end,
---        pinned = true,
---        collapsed = true, -- show window as closed/collapsed on start
---        open = "Neotree position=right git_status",
---      },
---      {
---        title = "Neo-Tree Buffers",
---        ft = "neo-tree",
---        filter = function(buf)
---          return vim.b[buf].neo_tree_source == "buffers"
---        end,
---        pinned = true,
---        collapsed = true, -- show window as closed/collapsed on start
---        open = "Neotree position=top buffers",
---      },
---      {
---        title = function()
---          local buf_name = vim.api.nvim_buf_get_name(0) or "[No Name]"
---          return vim.fn.fnamemodify(buf_name, ":t")
---        end,
---        ft = "Outline",
---        pinned = true,
---        open = "SymbolsOutlineOpen",
---      },
---      -- any other neo-tree windows
---      -- "neo-tree",
+        -- Neo-tree filesystem always takes half the screen height
+        {
+            title = "Neo-Tree",
+            ft = "neo-tree",
+            filter = function(buf)
+                return vim.b[buf].neo_tree_source == "filesystem"
+            end,
+            size = { height = 0.5 },
+        },
+        {
+            title = "Neo-Tree Git",
+            ft = "neo-tree",
+            filter = function(buf)
+                return vim.b[buf].neo_tree_source == "git_status"
+            end,
+            -- pinned = true,
+            collapsed = true, -- show window as closed/collapsed on start
+            open = "Neotree position=right git_status",
+        },
+        {
+            title = "Neo-Tree Buffers",
+            ft = "neo-tree",
+            filter = function(buf)
+                return vim.b[buf].neo_tree_source == "buffers"
+            end,
+            -- pinned = true,
+            collapsed = true, -- show window as closed/collapsed on start
+            open = "Neotree position=top buffers",
+        },
+        {
+            title = function()
+                local buf_name = vim.api.nvim_buf_get_name(0) or "[No Name]"
+                    return vim.fn.fnamemodify(buf_name, ":t")
+                end,
+            ft = "Outline",
+            -- pinned = true,
+            collapsed = true,
+            open = "SymbolsOutlineOpen",
+        },
     },
         right = {
             -- Add Copilot Chat to the right layout
