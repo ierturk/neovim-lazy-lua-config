14c4ab6364a416eb300fd175b80689596cbc9e17
Ibrahim Erturk <me@erturk.me>
2025-05-05 14:44:24 +0300

    Updated dap config

--- lua/config/plugins/nvim-dap.lua
+++ lua/config/plugins/nvim-dap.lua
@@ -2,8 +2,8 @@ return {
     {
         "mfussenegger/nvim-dap",
         dependencies = {
-            "rcarriga/nvim-dap-ui", -- Optional: UI for debugging
-            "theHamsta/nvim-dap-virtual-text", -- Optional: Inline variable display
+            "rcarriga/nvim-dap-ui", -- UI for debugging
+            "theHamsta/nvim-dap-virtual-text", -- Inline variable display
             "nvim-neotest/nvim-nio", -- Required dependency for nvim-dap-ui
         },
         config = function()
@@ -13,7 +13,7 @@ return {
             -- Configure LLDB adapter
             dap.adapters.lldb = {
                 type = "executable",
-                command = "/usr/local/bin/lldb-dap18", -- Replace with the full path to lldb-vscode17
+                command = "/usr/local/bin/lldb-dap18", -- Replace with the full path to lldb-vscode
                 name = "lldb",
             }
 
@@ -29,12 +29,57 @@ return {
                     cwd = "${workspaceFolder}",
                     stopOnEntry = true,
                     args = {}, -- Add program arguments here if needed
+                    runInTerminal = true, -- Run the app in the integrated terminal
                 },
             }
 
-            -- Optional: Configure UI
-            dapui.setup()
-            require("nvim-dap-virtual-text").setup()
+            -- Use the same configuration for C
+            dap.configurations.c = dap.configurations.cpp
+
+            -- Configure DAP UI
+            dapui.setup({
+                layouts = {
+                    {
+                        elements = {
+                            { id = "scopes", size = 0.25 },
+                            { id = "breakpoints", size = 0.25 },
+                            { id = "stacks", size = 0.25 },
+                            { id = "watches", size = 0.25 },
+                        },
+                        size = 40, -- Width of the DAP UI window
+                        position = "left", -- Position of the DAP UI window
+                    },
+                    {
+                        elements = {
+                            { id = "repl", size = 0.5 },
+                            { id = "console", size = 0.5 },
+                        },
+                        size = 10, -- Height of the DAP UI window
+                        position = "bottom", -- Position of the DAP UI window
+                    },
+                },
+            })
+
+            -- Automatically open/close DAP UI
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
+
+            -- Configure nvim-dap-virtual-text
+            require("nvim-dap-virtual-text").setup({
+                enabled = true, -- Automatically enable virtual text
+                enabled_commands = true, -- Enable commands like DapVirtualTextEnable
+                highlight_changed_variables = true, -- Highlight variables that have changed
+                highlight_new_as_changed = true, -- Highlight new variables as changed
+                show_stop_reason = true, -- Show the reason why the debugger stopped
+                commented = false, -- Do not prefix virtual text with comments
+            })
 
             -- Keybindings for debugging
             vim.keymap.set("n", "<F5>", function() dap.continue() end, { desc = "Start/Continue Debugging" })
@@ -45,6 +90,8 @@ return {
             vim.keymap.set("n", "<Leader>B", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, { desc = "Set Conditional Breakpoint" })
             vim.keymap.set("n", "<Leader>dr", function() dap.repl.open() end, { desc = "Open REPL" })
             vim.keymap.set("n", "<Leader>dl", function() dap.run_last() end, { desc = "Run Last Debugging Session" })
+            vim.keymap.set("n", "<Leader>du", function() dapui.toggle() end, { desc = "Toggle DAP UI" })
+            vim.keymap.set("n", "<Leader>dv", ":DapVirtualTextToggle<CR>", { desc = "Toggle DAP Virtual Text" })
         end,
     },
 }
